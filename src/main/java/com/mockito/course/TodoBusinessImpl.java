e72a031504811249ff91b5fc256f8ae972e1f0cd
Cesar Yona <cesaryona_@outlook.com>
2021-02-06 15:46:31 -0300

    ajustes

--- src/main/java/com/mockito/course/TodoBusinessImpl.java
+++ src/main/java/com/mockito/course/TodoBusinessImpl.java
@@ -11,13 +11,16 @@ public class TodoBusinessImpl {
 	private TodoService todoService;
 
 	public List<String> retrieveTodosRelatedToSpring(String user) {
+
 		List<String> filteredTodos = new ArrayList<String>();
 		List<String> allTodos = todoService.retrieveTodos(user);
+
 		for (String todo : allTodos) {
 			if (todo.contains("Spring")) {
 				filteredTodos.add(todo);
 			}
 		}
+		
 		return filteredTodos;
 	}
 }
